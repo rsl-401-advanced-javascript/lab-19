0b6c5c24809f53cf320afb24da31624400ec7c9e
RoryLetteney <rory.letteney@gmail.com>
2019-07-02 20:33:22 -0500

    Added files q

--- message-queue/index.js
+++ message-queue/index.js
@@ -5,5 +5,12 @@ Q.start();
 
 const dbQ = new Q('database');
 dbQ.monitorEvent('create');
+dbQ.monitorEvent('read');
 dbQ.monitorEvent('update');
 dbQ.monitorEvent('delete');
+
+const filesQ = new Q('files');
+filesQ.monitorEvent('save');
+filesQ.monitorEvent('error');
+
+module.exports = { dbQ, filesQ };
